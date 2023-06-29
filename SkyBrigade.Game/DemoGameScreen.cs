8eed977b520edb035907b6676c737de4dc7b0386
bogzxo <bogdanpechyorin@gmail.com>
2023-06-29 14:43:43 +0200

    Made the demo work with the new rendering framework🥶

--- SkyBrigade.Game/DemoGameScreen.cs
+++ SkyBrigade.Game/DemoGameScreen.cs
@@ -2,21 +2,23 @@
 using SkyBrigade.Engine;
 using SkyBrigade.Engine.Rendering;
 
+using Texture = SkyBrigade.Engine.OpenGL.Texture;
+
 namespace SkyBrigade.Game;
 
 class DemoGameScreen : IGameScreen
 {
     private RenderRectangle rect;
     private Camera testCamera;
+    private Texture testTexture;
 
     public void Initialize(GL gl)
     {
         gl.ClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
-        rect = new() {
-            Texture = GameManager.Instance.ContentManager.GenerateNamedTexture("amongus", "Assets/among.png")
-        };
-        
+        rect = new();
+        testTexture = GameManager.Instance.ContentManager.GenerateNamedTexture("amongus", "Assets/among.png");
+
         testCamera = new Camera() { Position = new System.Numerics.Vector3(0, 0, 5) };
     }
 
@@ -25,7 +27,7 @@ class DemoGameScreen : IGameScreen
         gl.Clear(ClearBufferMask.ColorBufferBit);
         gl.Viewport(0, 0, (uint)GameManager.Instance.Window.FramebufferSize.X, (uint)GameManager.Instance.Window.FramebufferSize.Y);
 
-        rect.Draw(testCamera);
+        rect.Draw(RenderOptions.Default with { Camera = testCamera, Texture = testTexture, Color = new System.Numerics.Vector4(MathF.Sin(timer * 0.5f), MathF.Sin(timer * 1.4f), MathF.Sin(timer), 1.0f) });
     }
 
     float timer = 0.0f;
@@ -35,7 +37,6 @@ class DemoGameScreen : IGameScreen
         testCamera.Update(dt);
 
         rect.Rotation += dt * 100.0f;
-        rect.Color = new System.Numerics.Vector4(MathF.Sin(timer * 0.5f), MathF.Sin(timer * 1.4f), MathF.Sin(timer), 1.0f);
     }
 
     public void Dispose()
